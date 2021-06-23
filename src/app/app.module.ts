732b589efa8177edf7bb08e7d8377742f4eb5d6c
James Skemp <strivinglife@gmail.com>
2021-06-22 22:06:50 -0500

    Add HttpClient support to application
    First part of step 6.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+// First step to make HttpClient available in the application.
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -20,7 +22,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Second step to make sure HttpClient is available everywhere in the application.
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
