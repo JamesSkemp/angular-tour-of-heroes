00d15e13c867616c41ac671761ebf276de2f4426
James Skemp <strivinglife@gmail.com>
2021-06-22 20:20:32 -0500

    Update with fourth part of step 5 changes
    Individual hero detail pages.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -11,9 +14,17 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero?: Hero;
 
-  constructor() { }
+  // ActivatedRoute = information about route to this component
+  // HeroService = hero data
+  // Location = Angular service for interacting with the browser
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,6 +16,13 @@ export class HeroService {
   // Angular will automatically inject the singleton MessageService.
   constructor(private messageService: MessageService) { }
 
+  getHero(id: number): Observable<Hero> {
+    // For now assume id is always correct
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
